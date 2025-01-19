87da526bcd3542067080907b9bbfacd5c8d3f477
alvanchumba <152489710+alvanchumba@users.noreply.github.com>
2025-01-19 13:27:59 +0200

    Add files via upload

--- basicnetwork_sniffer.py
+++ basicnetwork_sniffer.py
@@ -26,4 +26,31 @@ def get_mac_address(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return  ':'.join(bytes_str).upper()
 
+def ipv4_packet(data):
+    version_header_length = data[0] #bitwise operator tutorial
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target =struct.unpack( '! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:] 
+
+#return properly formattted ipv4
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+#unpack icmp packet
+def icmp_packet(data):
+    icmp_type, code, checksum =struct.unpack(' ! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack(' ! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_psh, flag_rst, flag_syn, flag_fin, data[offset: ]
+    
 main()
